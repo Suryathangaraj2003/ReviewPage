c89d3e57fd03284ed1b91a728a0a8654a222296d
surya thangaraj <suryathangaraj95@gmail.com>
2024-04-09 12:35:53 +0530

    second commit

--- src/components/Page7.jsx
+++ src/components/Page7.jsx
@@ -14,15 +14,17 @@ const Page7 = () => {
                 <h4 className='px-2 w-32 mt-[-20px] ml-8 text-center bg-yellow-500 border rounded-xl  sm:ml-12 md:ml-20 lg:ml-36 xl:ml-48
                 2xl:ml-60' >Most popular</h4>
                 <div className='flex flex-row'>
-                    <p className='w-32  mt-8 sm:w-full flex'>Foundation Math Build your foundation skills in algebra,geometry,and probability
-                    <img src={page7_img} alt="" srcset="" className='h-20 w-20 pr-6'/>
+                    <p className='  mt-8 sm:w-full sm:flex '>Foundation Math Build your foundation skills in algebra,geometry,and probability
+                    <img src={page7_img} alt="" srcset="" className='h-20 w-20  sm:mb-4'/>
                     </p>
                     </div>
                 
                 </div>
-                <div className='border-2 ml-2 mr-2 flex flex-row roundedhover:shadow-md hover:border-yellow-500 w-6/12 sm:mr-16 xl:mr-20 2xl:mr-32  rounded p-2 '>
-                    <p><b>Mathematical THinking</b> build your foundational skills in algebra,geometry and probability</p>
-                <img src={page7_img} alt="" srcset="" className='h-20 w-20 pr-6'/>
+                <div className='border-2 ml-2 mr-2 sm:flex flex-row roundedhover:shadow-md hover:border-yellow-500 w-6/12 sm:mr-16 xl:mr-20 2xl:mr-32  rounded p-2 '>
+                    <p className='pt-10'><b>Mathematical THinking</b> build your foundational skills in algebra,geometry and probability
+                    </p>
+                    <img src={page7_img} alt="" srcset="" className='h-20 w-20 '/>
+                
                 </div>
 
                 {/* <div className='border mx-4 flex flex-row pt-8  rounded hover:shadow-md hover:border-yellow-500'>
